38ceaad642a678dd6968cc448c34e637a1d7648d
Muhammad27770 <muhammadasad2777@gmail.com>
2025-05-05 14:29:11 +0500

    Delete student-service directory

--- student-service/src/main/java/com/example/student_service/repository/CourseDataRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.student_service.repository;
-
-import com.example.student_service.model.AcademicCourse;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseDataRepository extends JpaRepository<AcademicCourse, Long> {
-    AcademicCourse findByCourseTitleIgnoreCase(String title);
-}
